a415457638e003edde66af03f3ecb5ec64325451
bowenyou <=>
2021-03-29 21:00:07 -0400

    FIX: fixed COCOB optimizer bugs

--- COCOB/cocob.py
+++ COCOB/cocob.py
@@ -1,10 +1,12 @@
 import torch
 import torch.optim as optim
 
+
 class COCOB_Backprop(optim.Optimizer):
 
-    def __init__(self, params, alpha=100, epsilon=1e-8):
+    def __init__(self, params, device, alpha=100, epsilon=1e-8):
 
+        self.device = device
         self.alpha = alpha
         self.epsilon = epsilon
         defaults = dict(alpha=alpha, epsilon=epsilon)
@@ -24,20 +26,26 @@ class COCOB_Backprop(optim.Optimizer):
                 state = self.state[p]
 
                 if len(state) == 0:
-                    state['theta'] = torch.zeros_like(p.data)
-                    state['G'] = torch.zeros_like(p.data)
-                    state['L'] = self.epsilon * torch.ones_like(p.data)
-                    state['w'] = torch.zeros_like(p.data)
-                    state['reward'] = torch.zeros_like(p.data)
+                    state['theta'] = torch.zeros_like(
+                        p.data, device=self.device)
+                    state['G'] = torch.zeros_like(p.data, device=self.device)
+                    state['L'] = self.epsilon * \
+                        torch.ones_like(p.data, device=self.device)
+                    state['w'] = torch.zeros_like(p.data, device=self.device)
+                    state['reward'] = torch.zeros_like(
+                        p.data, device=self.device)
 
                 state['L'] = state['L'].max(grad.abs())
                 state['G'] = state['G'] + grad.abs()
-                state['reward'] += (p.data - state['w']).mul(grad)
-                state['reward'] = state['reward'].max(torch.zeros(1))
+                state['reward'] -= state['w'].mul(grad)
+                state['reward'] = state['reward'].max(
+                    torch.zeros(1, device=self.device))
                 state['theta'] += grad
 
                 temp = (self.alpha * state['L']).max(state['G'] + state['L'])
-                beta = state['theta'].div(state['L']).div(temp)
+                beta = -state['theta'].div(state['L']).div(temp)
 
-                p.data = state['w'] - beta.mul(state['L'] + state['reward'])
+                p.data -= state['w'] - beta.mul(state['L'] + state['reward'])
+                state['w'] = p.data
+
         return loss
